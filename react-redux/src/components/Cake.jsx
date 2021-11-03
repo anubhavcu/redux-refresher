6dd3abc39fb64b5460e8efda902aa31bc878873e
anubhavcu <anubhav342y@gmail.com>
2021-11-03 17:01:29 +0530

    class alternative

--- react-redux/src/components/Cake.jsx
+++ react-redux/src/components/Cake.jsx
@@ -1,24 +1,55 @@
 import { useState } from 'react';
 import { connect } from 'react-redux';
 import { buyCake } from '../redux/actions/cakeAction';
-const Cake = (props) => {
-  const [number, setNumber] = useState(1);
-  return (
-    <div>
-      <h2>Number of Cakes - {props.cakes} </h2>
-      <input
-        type='number'
-        name=''
-        id=''
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
-      />
-      <button onClick={() => props.buyCake(number)}>
-        Buy {number} of Cake
-      </button>
-    </div>
-  );
-};
+// const Cake = (props) => {
+//   const [number, setNumber] = useState(1);
+//   return (
+//     <div>
+//       <h2>Number of Cakes - {props.cakes} </h2>
+//       <input
+//         type='number'
+//         name=''
+//         id=''
+//         value={number}
+//         onChange={(e) => setNumber(e.target.value)}
+//       />
+//       <button onClick={() => props.buyCake(number)}>
+//         Buy {number} of Cake
+//       </button>
+//     </div>
+//   );
+// };
+
+import React, { Component } from 'react';
+
+export class Cake extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 1,
+    };
+  }
+
+  render() {
+    const { number } = this.state;
+    return (
+      <div>
+        <h2>Number of cakes {this.props.cakes}</h2>
+        <input
+          type='number'
+          name=''
+          id=''
+          value={number}
+          onChange={(e) => this.setState({ number: e.target.value })}
+        />
+        <button onClick={() => this.props.buyCake(number)}>
+          Buy {number} of Cake
+        </button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
